54338f1d35dbdfe18109b86130a1f82f31321c80
refugiorangel <54346027+refugiorangel@users.noreply.github.com>
2023-12-12 16:20:33 -0600

    Update test.py

--- codigo/test.py
+++ codigo/test.py
@@ -9,7 +9,7 @@ import paramiko
 import os
 
 activeMachines = []
-allMachines = ["30","31","32","33","34","35"]
+allMachines = ["30","31","32","33","34","35","36"]
 port = 1234
 localIP = socket.gethostbyname(socket.gethostname()).split(".")[-1]
 masterNode = ""
