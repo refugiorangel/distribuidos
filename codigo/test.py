6508aa30e16e68a1bab0c3bb5fccac170aab9c18
refugiorangel <54346027+refugiorangel@users.noreply.github.com>
2023-12-13 11:01:37 -0600

    Update test.py

--- codigo/test.py
+++ codigo/test.py
@@ -11,7 +11,7 @@ import warnings
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 activeMachines = []
-allMachines = ["30","31","32","33","34","35","36"]
+allMachines = ["30","31","32","33","34","35","36","37","38","39",]
 port = 1234
 localIP = socket.gethostbyname(socket.gethostname()).split(".")[-1]
 masterNode = ""
@@ -116,14 +116,45 @@ def analizedMessage(message,server):
                 else:
                     cantidad = df.at[indice_fila, 'Existencias']
                     df.at[indice_fila, 'Existencias'] = df.at[indice_fila, 'Existencias'] - cantidad
-                df.to_csv(path, index=False)
+                df.to_csv(general, index=False)
 
                 total_costo = float(tokens[3]) * float(linea['Price'])
                 f = open("/home/adm-user1/proyecto/distribuidos/movimientos/movimientos.csv", "a")
                 f.write(f"{tokens[2]},{tokens[1]},{tokens[4]},{cantidad},{total_costo}\n")
-                print(f"{tokens[2]},{tokens[1]},{tokens[4]},{cantidad},{total_costo}")
+                print("ORDER SELL CORRECT!!!")
             except:
-                pass
+                print("ORDER SELL FAILED!!!")
+
+        case "A7":
+            path = f"/home/adm-user1/proyecto/distribuidos/productos/"
+            general = f"/home/adm-user1/proyecto/distribuidos/productos/productos.csv" 
+            try:
+                cantidad = int(tokens[7]) // (len(activeMachines) +1)
+                f = open(general, "a")
+                f.write(f"{tokens[1]},{tokens[2]},{tokens[3]},{tokens[4]},{tokens[5]},{tokens[6]},{tokens[7]}\n")
+                f.close()
+                f = open(path + localIP + ".csv", "a")
+                f.write(f"{tokens[1]},{tokens[2]},{tokens[3]},{tokens[4]},{tokens[5]},{tokens[6]},{cantidad}\n")
+                f.close()
+                for i in activeMachines:
+                    f = open(path+i+".csv" ,"a")
+                    f.write(f"{tokens[1]},{tokens[2]},{tokens[3]},{tokens[4]},{tokens[5]},{tokens[6]},{cantidad}\n")
+                print("LOAD PRODUCT SUCCESS")
+            except:
+                print("LOAD PRODUCT FAILED")
+
+        case "A8":
+            general = f"/home/adm-user1/proyecto/distribuidos/clientes/clientes.csv" 
+            try:
+                f = open(general, "a")
+                f.write(f"{tokens[1]},{tokens[2]},{tokens[3]},{tokens[4]},{tokens[5]}\n")
+                f.close()
+                print("LOAD CLIENT SUCCESS")
+            except:
+                print("LOAD CLIENT FAILED")
+
+        case "A9":
+            pass
 
         case "FF":
             pass
@@ -132,6 +163,7 @@ def activeServer():
     global ipBase
     global port
     global localIP
+    global activeMachines
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((ipBase + localIP, port))
     server.listen(5)
@@ -139,7 +171,11 @@ def activeServer():
     while True:
         conn, cliente = server.accept()
         print(f"NEW CONNECTION FROM {cliente[0]}")
-        message = conn.recv(20).decode('utf-8')
+        tokens = cliente[0].split(".")
+        if tokens[-1] not in activeMachines:
+            activeMachines.add(cliente[0])
+            print(f"{tokens[-1]} ADD TO ACTIVEMACHINES")
+        message = conn.recv(1024).decode('utf-8')
         if message != "":
             print(f"RECEIVEC {message} from {cliente[0]}")
             analizedMessage(message, server)
@@ -151,12 +187,16 @@ def activeServer():
 def sendMessage(ip, message):
     global ipBase
     global port
+    global activeMachines
     print(f"SENDING {message} to {ip}")
     try:
         cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         cliente.connect((ipBase + ip, port))
         cliente.send(message.encode('utf-8'))
-        response = cliente.recv(20).decode('utf-8')
+        if ip not in activeMachines:
+            activeMachines.add(ip)
+            print(f"{ip} ADD ACTIVE DIRECTORY")
+        response = cliente.recv(1024).decode('utf-8')
         analizedMessage(response, cliente)
         print(f"SEND {message} to {ip} SUCESS!!!")
     except:
@@ -211,18 +251,19 @@ def sendActive():
     global activeMachines
 
     if(threading.active_count() == 3):
-        try:
-            sendMessage(masterNode, f"A4 {localIP}")
-        except:
-            index = activeMachines.index(i)
-            activeMachines.remove(index)
-            sendAll(f"A1 {masterNode}")
-            activeMachines.sort()
-            if((len(activeMachines) -1) == activeMachines.index(localIP)):
-                next = activeMachines[0]
-            else:
-                next = activeMachines[activeMachines.index(localIP) + 1]
-            sendMessage(next, f"A3 {localIP}")
+        if masterNode != localIP:
+            try:
+                sendMessage(masterNode, f"A4 {localIP}")
+            except:
+                index = activeMachines.index(i)
+                activeMachines.remove(index)
+                sendAll(f"A1 {masterNode}")
+                activeMachines.sort()
+                if((len(activeMachines) -1) == activeMachines.index(localIP)):
+                    next = activeMachines[0]
+                else:
+                    next = activeMachines[activeMachines.index(localIP) + 1]
+                sendMessage(next, f"A3 {localIP}")
     else:
         pass
 
@@ -234,7 +275,7 @@ def defineMaster(ip):
 
 def initialDistribution():
     global activeMachines  
-    global localIP  
+    global localIP 
     df = pd.read_csv("/home/adm-user1/proyecto/distribuidos/productos/productos.csv")
     if len(activeMachines) >0:
         df["Existencias"] = df["Existencias"] // (len(activeMachines)+1)
@@ -246,6 +287,7 @@ def initialDistribution():
         pf.to_csv(f"/home/adm-user1/proyecto/distribuidos/productos/{localIP}.csv", index=False)
     else:
         df.to_csv(f"/home/adm-user1/proyecto/distribuidos/productos/{localIP}.csv", index=False)
+    sendAll(f"A2 {localIP}")
     sendFiles()
     print("ALL FILES SENDING")
     
@@ -313,10 +355,45 @@ def sell(ip, itemid, cantidad, cliente):
         total_costo = float(cantidad) * float(linea['Price'])
         f = open("/home/adm-user1/proyecto/distribuidos/movimientos/movimientos.csv", "a")
         f.write(f"{ip},{itemid},{cliente},{cantidad},{total_costo}\n")
-        print(f"{ip},{itemid},{cliente},{cantidad},{total_costo}")
+        print("ORDER SELL CORRECT!!")
     except:
         pass
 
+def addProduct(ItemID,ItemBarcode,ItemName,Price,Cost,Categoria,Existencias):
+    global activeMachines
+    global localIP
+    path = f"/home/adm-user1/proyecto/distribuidos/productos/"
+    general = f"/home/adm-user1/proyecto/distribuidos/productos/productos.csv" 
+    message = f"A7 {ItemID} {ItemBarcode} {ItemName} {Price} {Cost} {Categoria} {Existencias}"
+    sendAll(message)
+    try:
+        cantidad = int(Existencias) // (len(activeMachines) +1)
+        f = open(general, "a")
+        f.write(f"{ItemID},{ItemBarcode},{ItemName},{Price},{Cost},{Categoria},{Existencias}\n")
+        f.close()
+        f = open(path + localIP + ".csv", "a")
+        f.write(f"{ItemID},{ItemBarcode},{ItemName},{Price},{Cost},{Categoria},{cantidad}\n")
+        f.close()
+        for i in activeMachines:
+            f = open(path+i+".csv" ,"a")
+            f.write(f"{ItemID},{ItemBarcode},{ItemName},{Price},{Cost},{Categoria},{cantidad}\n")
+        print("LOAD PRODUCT SUCCESS")
+    except:
+        print("LOAD PRODUCT FAILED")
+
+def addClient(ClienteID,Nombre,Apellido,Email,Telefono):
+    global activeMachines
+    global localIP
+    general = f"/home/adm-user1/proyecto/distribuidos/cleitnes/clientes.csv" 
+    message = f"A8 {ClienteID} {Nombre} {Apellido} {Email} {Telefono}"
+    sendAll(message)
+    try:
+        f = open(general, "a")
+        f.write(f"{ClienteID},{Nombre},{Apellido},{Email},{Telefono}\n")
+        f.close()
+        print("LOAD CLIENT SUCCESS")
+    except:
+        print("LOAD CLIENT FAILED")
 
 typeAction = {
     "00": {
@@ -356,8 +433,44 @@ typeAction = {
 
     "07": {
         "function": sell,
-        "def": "copia un archivo a otra maquina"
+        "def": "Registra la venta de un producto"
     },
+
+    "08": {
+        "function": addProduct,
+        "def": "agrega un producto y lo distribuye"
+    },
+
+    "09": {
+        "function": addClient,
+        "def": "agrega un cliente"
+    },
+
+    "0A": {
+        "function": None,
+        "def": "agrega un cliente"
+    },
+
+    "0B": {
+        "function": None,
+        "def": "agrega un cliente"
+    },
+
+    "0C": {
+        "function": None,
+        "def": "agrega un cliente"
+    },
+
+    "0D": {
+        "function": None,
+        "def": "agrega un cliente"
+    },
+
+    "0E": {
+        "function": None,
+        "def": "agrega un cliente"
+    },
+
 }
 
 server_t = threading.Thread(target=activeServer)
@@ -396,6 +509,42 @@ while True:
             sp = inp.split(",")
             action_t = threading.Thread(target=accion, args=(sp[0], sp[1], sp[2], sp[3]))
             action_t.start()
+
+        case "08":
+            inp = input("ItemID,ItemBarcode,ItemName,Price,Cost,Categoria,Existencias:")
+            sp = inp.split(",")
+            action_t = threading.Thread(target=accion, args=(sp[0], sp[1], sp[2], sp[3], sp[4], sp[5], sp[6]))
+            action_t.start()
+        #agregar inventario general
+
+        case "09":
+            inp = input("ClienteID,Nombre,Apellido,Email,Telefono:")
+            sp = inp.split(",")
+            action_t = threading.Thread(target=accion, args=(sp[0], sp[1], sp[2], sp[3], sp[4]))
+            action_t.start()
+        #agregar cliente
+
+        case "0A":
+            print(masterNode)
+
+        case "0B":
+            print(activeMachines)
+
+        case "0C":
+            path = "/home/adm-user1/proyecto/distribuidos/cleitnes/clientes.csv"
+            df = pd.read_csv(path)
+            print(df)
+
+        case "0D":
+            path = "/home/adm-user1/proyecto/distribuidos/productos/productos.csv"
+            df = pd.read_csv(path)
+            print(df)
+    
+        case "0E":
+            inp = input("Sucursal:")
+            path = f"/home/adm-user1/proyecto/distribuidos/productos/{inp}.csv"
+            df = pd.read_csv(path)
+            print(df)
 
         case _:
             action_t = threading.Thread(target=accion)
